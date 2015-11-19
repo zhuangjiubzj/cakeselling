dfb2cb97220daf46b54bfe3a195f9df97fd740eb
Jiu Zhuang <zhuangjiubzj@qq.com>
2015-11-19 08:09:29 +0800

    Skeleton User profile page support

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :users  
   root 'static_pages#home'
-  
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
