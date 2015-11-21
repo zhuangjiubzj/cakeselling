f502e3fc79f198c8ed4878e59bf1f13a26e49e1a
Jiu Zhuang <zhuangjiubzj@qq.com>
2015-11-22 03:53:08 +0800

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
        secure_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
        @user = User.new(params[:user])
         if @user.save
-        flash[:success] = "Welcome to the Twitter App!"
+        remember @user
+        flash[:success] = "Welcome to the Sample App!"
         redirect_to @user
         else
         render 'new' 
